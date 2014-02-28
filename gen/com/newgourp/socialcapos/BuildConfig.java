3f1d7e054f7dff1c877877a4f89e67bafbc2ea1a
Philipp Streicher <philippstr@gmx.de>
2014-02-28 23:12:05 +0100

    Initialize

---
+++ gen/com/newgourp/socialcapos/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.newgourp.socialcapos;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/newgourp/socialcapos/MainActivity.java
@@ -0,0 +1,15 @@
+package com.newgourp.socialcapos;
+import org.apache.cordova.Config;
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap{
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl(Config.getStartUrl());
+	}
+}
