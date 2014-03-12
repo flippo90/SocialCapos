6e45243459f3d9a7f250b9cb51c7a1226ed924b1
Philipp Streicher <philippstr@gmx.de>
2014-03-12 22:20:22 +0100

    eventDetails, select location on map, current pos

---
+++ assets/www/js/eventDetails.js
@@ -0,0 +1,17 @@
+
+function init(){
+	
+	// here we have to get the information for the specific event that is at the location with the koordinated given
+	// with the link at the hash tag...
+	
+	// this are the koordinates of the bar. we can search for the location with them
+	var coordinates = window.location.hash.slice(1);
+	
+	//the information from the event that takes place
+	var geoLocationLabel = document.getElementById("geoLocationInput").innerHTML = coordinates;
+	var locationNameLabel = document.getElementById("locationName").innerHTML  = "Capos";
+	var partyNameLabel = document.getElementById("partyName").innerHTML  = "Geilste Party ever";
+	var specialsLabel = document.getElementById("specials").innerHTML  = "2 Cocktails zum preis von einem!!!";
+	var numberLikesLabel = document.getElementById("numberLikes").innerHTML  = "253";
+	var openingTimeLabel = document.getElementById("openingTime").innerHTML  = "22-3 Uhr";
+}
--- assets/www/js/locationOverview.js
+++ assets/www/js/locationOverview.js
@@ -1,18 +1,40 @@
-var mapOptions = {
-	center: new google.maps.LatLng(48.400884, 9.991201000000046/*get my geo location*/),
-	zoom: 15
-};
-
 var map;
-
+var myGeoLoaction;
 function initialize() {
 
+	// init google maps
+	var mapOptions = {
+			center: new google.maps.LatLng(60, 105),
+			zoom: 14
+		};
+		
 	map = new google.maps.Map(document.getElementById('map-canvas'),
 		mapOptions);
-
+	
+	//find my location and set marker to it
+	markCurrentLocation();
+	
 	//foreach location in database
-		var lgn = new google.maps.LatLng(48.400884, 9.991201000000046);
-		createMarker(lgn);
+	var lgn = new google.maps.LatLng(48.400884, 9.991201000000046);
+	createMarker(lgn);
+}
+
+function markCurrentLocation()
+{
+	if (navigator.geolocation)
+	{
+		console.log("try to get location");
+		navigator.geolocation.getCurrentPosition(showPosition);
+	}
+	else{console.log("Geolocation is not supported by this browser.");}
+}
+
+function showPosition(position)
+{
+	console.log("show position: " + position.coords.latitude + "," + position.coords.longitude);
+	var myPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	map.setCenter(myPos);
+	createMarker(myPos);
 }
 
 function createMarker(latLng){
@@ -22,9 +44,23 @@ function createMarker(latLng){
 	});
 	
 	google.maps.event.addListener(marker, 'click', function() {
-	     console.log("Marker clicked: " + marker.getPosition());
 	     //show event details page
-	     window.open("eventDetails.html","_self")
+	     window.open("eventDetails.html?#" + marker.getPosition() ,"_self");
+	  });
+}
+
+function createCurrentPosMarker(latLng){
+	var image = 'homer_simpson_on_a_chair.jpg';
+	
+	var marker = new google.maps.Marker({
+		position: latLng,
+		map: map,
+		icon:image,
+	});
+	
+	google.maps.event.addListener(marker, 'click', function() {
+	     //show event details page
+	     window.open("eventDetails.html?#" + marker.getPosition() ,"_self");
 	  });
 }
 
